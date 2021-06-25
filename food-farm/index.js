bd84b68f7057de62aca7c5ab3c826f1aa2126b86
Subodh Sharma <81741899+subodhsharmaofficial@users.noreply.github.com>
2021-06-25 14:52:01 +0530

    Update index.js

--- food-farm/index.js
+++ food-farm/index.js
@@ -99,6 +99,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+// server.listen(8000, '127.0.0.1', () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log('Server is loading...');
 });
